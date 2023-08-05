cffdc8ec5b1ed977d54599dea4bf776e2ba23dae
Afnan011 <muhammedafnan8184@gmail.com>
2023-08-05 15:49:35 +0530

    Dictionary in dart examples added.

--- lib/samples/Example1.dart
+++ lib/samples/Example1.dart
@@ -4,6 +4,53 @@ void main()
 {
   print("hello world");
 
+  // listExamples();
+  mapExamples();
+
+
+}
+
+void mapExamples() {
+  //method-1
+//   var afnan = {
+//     'name': 'Afnan',
+//     'usn':"4VP22MC027",
+//     'm1':89
+//   };
+//
+//   print("name : ${afnan['name']}, USN: ${afnan['usn']}, Marks: ${afnan['m1']}");
+//
+//    afnan['m1'] = 91;
+//    print('Marks: ${afnan['m1']}');
+
+//   method-2
+
+  // var afnan = Map();
+  // afnan['name'] = "Afnan";
+  // afnan['usn'] = "4VP22MC027";
+  // afnan['m1'] = 90;
+  //
+  // print(afnan);
+  //
+  // print(afnan.isEmpty);
+  // print(afnan.isNotEmpty);
+  // print(afnan.containsKey('name'));
+  // print(afnan.containsKey('nn'));
+  // print(afnan.containsValue('afnan'));
+  // print(afnan.containsValue('Afnan'));
+  // print(afnan.keys);
+  // print(afnan.remove('name'));
+  // print(afnan);
+
+
+
+
+
+}
+
+
+void listExamples()
+{
   var list1 = [1, 2, 3, 4, 5];
 
   var list2 = [];
@@ -27,5 +74,4 @@ void main()
   print("is Empty?: ${list2.isEmpty}");
   print("is not Empty?: ${list2.isNotEmpty}");
   print("element at index 2: ${list2.elementAt(2)}");
-
 }
